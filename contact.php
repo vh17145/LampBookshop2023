2205e161830912b93d72bbe444666d2077a4eec6
vh17145 <110210976+vh17145@users.noreply.github.com>
2023-06-23 09:41:36 +1200

    Added a google maps link to the Lamp address
    My client said they would like to have a google maps frame to show where Lamp looks is and so i added it here.

--- contact.php
+++ contact.php
@@ -18,7 +18,13 @@
          <div class="row">
         <div class="leftside">
             <h3>Lamp contact details</h3>
-       
+       <p>Contact Lamp Bookshop through our email address:<br>
+        Our phone number: 027 222 8228<br>
+        Or come in store at 595 MacKay Street, Thames 3500<br>
+            </p>
+            <div class="googlemap">
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1590.3199051210213!2d175.54151053856998!3d-37.13748345480851!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d72813798c04805%3A0x772ab7887f33edef!2sLamp%20Books!5e0!3m2!1sen!2snz!4v1687394380894!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
         </div>
        
         <div class ="rightside">
